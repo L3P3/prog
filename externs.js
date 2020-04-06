df00f36968baa5c6ba88f83bbe48ab913d5bf25b
Lennard Peschko <lennard.peschko@tudock.de>
2020-04-06 10:45:00 +0200

    added missing component lifecycle methods

--- externs.js
+++ externs.js
@@ -12,7 +12,13 @@ var TYPE_VNODE;
 
 /**
 @typedef {{
-	view: function(TYPE_VNODE)
+	view: function(TYPE_VNODE),
+	oninit: function(TYPE_VNODE),
+	oncreate: function(TYPE_VNODE),
+	onupdate: function(TYPE_VNODE),
+	onbeforeremove: function(TYPE_VNODE):Promise,
+	onremove: function(TYPE_VNODE),
+	onbeforeupdate: function(TYPE_VNODE, TYPE_VNODE): boolean
 }}
 */
 var TYPE_COMPONENT;
